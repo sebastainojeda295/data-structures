45f0d810368dc801f23c547dfaf9bef406542f10
sebastian <sebastianojeda295@gmail.com>
2026-09-08 15:24:05 -0500

    upload folder projects

---
+++ java/Hello/src/main/java/com/mycompany/hello/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hello;
+
+/**
+ *
+ * @author Lab Analisis Datos
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
